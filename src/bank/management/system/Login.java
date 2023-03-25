403471263b4ddbcdcde98c8d2249e113366371e1
Khushbul <Khushbul@DESKTOP-HR6P5UN>
2023-03-25 19:22:54 +0600

    First commit

--- src/bank/management/system/Login.java
+++ src/bank/management/system/Login.java
@@ -2,10 +2,24 @@
 package bank.management.system;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Login extends JFrame {
+    Login(){
+        
+        setTitle("AUTOMATED TALLER SYSTEM");
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel label = new JLabel (i3);
+        add(label);
+        
+        setSize(800,480);
+        setVisible(true);
+        setLocation(350, 200);
+    }
     
     public static void main(String args[]){
-        
+        new Login();
     }
 }
